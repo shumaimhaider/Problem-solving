694517b7b28568232923b0ca2051d0b20bf407ae
shumaimhaider <shumaimhaider@gmail.com>
2024-06-24 10:33:22 +0500

    Q-804. Unique Morse Code Words

---
+++ BruteForce/Q_804_unique_morse_code.js
@@ -0,0 +1,51 @@
+/** 
+ *  https://leetcode.com/problems/unique-morse-code-words/
+ *Q-804 International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+    let transformations = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    const hash = new Map()
+
+    for (let i = 0; i < alphabets.length; i++) {
+        hash.set(alphabets[i], transformations[i])
+    }
+
+    let result = new Set()
+    for (let word of words) {
+        let morseCode = ''
+        for (let chr of word) {
+            morseCode += hash.get(chr)
+        }
+        result.add(morseCode)
+    }
+
+    return result.size
+};
