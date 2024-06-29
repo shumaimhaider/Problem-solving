dcefbc41770ac6656f62c4527099202343ecad13
shumaimhaider <shumaimhaider@gmail.com>
2024-06-29 21:43:04 +0500

    Q-1748: Sum of Unique Elements

---
+++ BruteForce/Q_1748_sum_of_unique_elements.js
@@ -0,0 +1,25 @@
+/** Q-1748 Sum of Unique Elements
+ * https://leetcode.com/problems/sum-of-unique-elements/submissions/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+    let hash = new Map()
+    let sum = 0
+    for(let i = 0; i < nums.length; i++){
+        if (hash.get(nums[i])){
+             hash.set(nums[i], hash.get(nums[i])+1)
+        }else{
+            hash.set(nums[i], 1)
+        }
+    }
+    
+    for(let[key, count] of hash){
+        if (count == 1){
+            sum += key
+        }
+    }
+    
+    return sum
+    
+};
